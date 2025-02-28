434ec620da645c2a7a501de69bf7b168b54a19bf
BitsV3rm <66098177+BitsV3rm@users.noreply.github.com>
2025-02-28 08:41:13 +0800

    Update ui_only.lua

--- ui_only.lua
+++ ui_only.lua
@@ -1,1 +1,418 @@
+getgenv().Settings = {
+	Enemies = { 
+        ["Silver Lake"] = {
+            ["Mobs"] = {
+                "Koromon",
+                "Tanemon",
+                "Pagumon",
+                "Betamon",
+                "Shellmon",
+                "Goblinmon"
+            },
+            ["Bosses"] = {
+                "Kuwagamon",
+                "Seadramon"
+            }
+        },
+        ["Gear Savannah"] = {
+            ["Mobs"] = {
+                "Chuumon",
+                "Monochromon",
+                "Hagurumon",
+                "Kabuterimon"
+            },
+            ["Bosses"] = {
+                "Meramon",
+                "Ogremon",
+                "Leomon"
+            }
+        },
+        ["Infinite Mountain"] = {
+            ["Mobs"] = {
+                "Bakemon",
+                "DarkTyrannomon",
+                "DemiDevimon",
+                "Icemon",
+                "Strikedramon"
+            },
+            ["Bosses"] = {
+                "Unimon",
+                "WaruMonzaemon"
+            }
+        }
+    },
+	placeVer = {
+        [133649758958568] = 2389,  -- Silver Lake
+        [92975923292118] = 865,    -- Gear Savannah
+        [138083468820287] = 702,   -- Infinite Mountain
+        [80299472659017] = 894,    -- Colosseum_Easy
+        [76011326497329] = 243,    -- GoblinFort_Normal
+        [86392425558311] = 201     -- GoblinFort_Hard
+    },
+	placeId = {
+		["Silver Lake"] = 133649758958568,  -- Silver Lake
+        ["Gear Savannah"] = 92975923292118,    -- Gear Savannah
+        ["Infinite Mountain"] = 138083468820287,   -- Infinite Mountain
+        ["Colosseum_Easy"] = 80299472659017,    -- Colosseum_Easy
+        ["GoblinFort_Normal"] = 76011326497329,    -- GoblinFort_Normal
+        ["GoblinFort_Hard"] = 86392425558311     -- GoblinFort_Hard
+	},
+	Maps = {
+        ["Silver Lake"] = Vector3.new(-60.7276, -363.8864, -203.6405),
+        ["Gear Savannah"] = Vector3.new(-980.0617065429688, -459.4217834472656, 610.3125610351562),
+        ["Infinite Mountain"] = Vector3.new(-626.793212890625, -21.54945182800293, 898.1337280273438)
+    }
+}
 
+
+local MacLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/BitsV3rm/rblx_ui/refs/heads/main/maclib.txt"))()
+local player = game:GetService("Players").LocalPlayer
+local UIisLoaded = 0
+
+--- FUNCTIONS ---
+local function saveConfig()
+	MacLib:SaveConfig("bitsv3rm")
+end
+--- FUNCTIONS ---
+
+--- UI ---
+local Window = MacLib:Window({
+	Title = "Digimon: Reboot World",
+	Subtitle = "FARM | DUNGEON | COLOSSEUM",
+	Size = UDim2.fromOffset(868, 650),
+	DragStyle = 1,
+	DisabledWindowControls = {},
+	ShowUserInfo = true,
+	Keybind = Enum.KeyCode.LeftControl,
+	AcrylicBlur = true,
+})
+
+local Group_Main = Window:TabGroup() -- Main Group
+local Tab_Main = Group_Main:Tab({ -- Main Tab
+    Name = "Main",
+    Image = "rbxassetid://80106645720330"
+})
+local Section_Main_Enable = Tab_Main:Section({ -- Main Enable Section
+    Side = "Left"
+})
+Section_Main_Enable:Toggle({ -- Main Enable Toggle
+	Name = "Enable",
+	Default = false,
+	Callback = function()
+		if UIisLoaded < 2 then 
+			return 
+		end
+		
+		if MacLib.Options["EnabledButton"].State then
+			saveConfig()
+		end
+	end,
+}, "EnabledButton")
+local Section_Main_Functions = Tab_Main:Section({ -- Main Function Section
+    Side = "Left"
+})
+Section_Main_Functions:Toggle({ -- Main Auto-Farm Toggle
+	Name = "Auto-Farm",
+	Default = false,
+	Callback = function()
+		repeat task.wait() until UIisLoaded == 2
+		
+		if MacLib.Options["AutoFarm_Toggle"].State then
+			MacLib.Options["AutoDungeon_Toggle"]:UpdateState(false)
+			MacLib.Options["AutoColosseum_Toggle"]:UpdateState(false)
+			saveConfig()
+			if not MacLib.Options["EnabledButton"].State or (not (MacLib.Options["AutoFarm_Bot_Toggle"].State and MacLib.Options["mobs_Dropdown"].Value) and not MacLib.Options["AutoFarm_Macro_Toggle"].State) then
+				MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+				saveConfig()
+				return
+			end
+
+			while task.wait(0.1) and MacLib.Options["EnabledButton"].State and MacLib.Options["AutoFarm_Toggle"].State and (MacLib.Options["AutoFarm_Bot_Toggle"].State or MacLib.Options["AutoFarm_Macro_Toggle"].State) do
+				checkPlaceId(MacLib.Options["world_Dropdown"].Value)
+				auto_Farm()
+			end
+		end
+	end,
+}, "AutoFarm_Toggle")
+Section_Main_Functions:Toggle({ -- Main Auto-Dungeon Toggle
+	Name = "Auto-Dungeon",
+	Default = false,
+	Callback = function()
+		repeat task.wait() until UIisLoaded == 2
+
+		if MacLib.Options["AutoDungeon_Toggle"].State then
+			MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+			MacLib.Options["AutoColosseum_Toggle"]:UpdateState(false)
+			saveConfig()
+		end
+	end,
+}, "AutoDungeon_Toggle")
+Section_Main_Functions:Toggle({ -- Main Auto-Colosseum Toggle
+	Name = "Auto-Colosseum",
+	Default = false,
+	Callback = function()
+		repeat task.wait() until UIisLoaded == 2
+
+		if MacLib.Options["AutoColosseum_Toggle"].State then
+			MacLib.Options["AutoDungeon_Toggle"]:UpdateState(false)
+			MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+			saveConfig()
+		end
+	end,
+}, "AutoColosseum_Toggle")
+
+local Section_Main_Save = Tab_Main:Section({ -- Main Enable Section
+    Side = "Right"
+})
+Section_Main_Save:Button({
+	Name = "Save Config",
+	Callback = function()
+		MacLib:SaveConfig("bitsv3rm")
+	end,
+})
+
+local Group_Function = Window:TabGroup() -- Function Group
+local Tab_AutoFarm = Group_Function:Tab({ -- Function Auto-Farm Tab
+    Name = "Auto-Farm",
+    Image = "rbxassetid://123447833037199"
+})
+
+local Section_AutoFarm_Bot = Tab_AutoFarm:Section({ --
+    Side = "Left"
+})
+Section_AutoFarm_Bot:Header({
+	Text = "Bot Mode"
+}, "")
+Section_AutoFarm_Bot:Toggle({ -- Main Auto-Colosseum Toggle
+	Name = "Enable",
+	Default = false,
+	Callback = function()
+		if UIisLoaded < 2 then 
+			return 
+		end
+
+		if MacLib.Options["AutoFarm_Bot_Toggle"].State then
+			if MacLib.Options["AutoFarm_Macro_Toggle"].State then
+				MacLib.Options["AutoFarm_Macro_Toggle"]:UpdateState(false)
+			end
+			if MacLib.Options["AutoFarm_Toggle"].State then
+				MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+			end
+			saveConfig()
+		elseif not MacLib.Options["AutoFarm_Bot_Toggle"].State and not MacLib.Options["AutoFarm_Macro_Toggle"].State and MacLib.Options["AutoFarm_Toggle"].State then
+			MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+			saveConfig()
+		end
+	end,
+}, "AutoFarm_Bot_Toggle")
+Section_AutoFarm_Bot:Dropdown({
+	Name = "World",
+	Search = false,
+	Multi = false,
+	Required = true,
+	Options = {"Silver Lake", "Gear Savannah", "Infinite Mountain"},
+	Default = 1,
+	Callback = function(World)
+		if UIisLoaded < 2 then 
+			return 
+		end
+		warn("fasdfasdfasdfasd")
+		MacLib.Options["mobs_Dropdown"]:ClearOptions()
+		MacLib.Options["mobs_Dropdown"].Value = nil
+		MacLib.Options["mobs_Dropdown"]:InsertOptions(getgenv().Settings.Enemies[World]["Mobs"])
+		saveConfig()
+	end,
+}, "world_Dropdown")
+Section_AutoFarm_Bot:Dropdown({
+	Name = "Mobs",
+	Search = true,
+	Multi = true,
+	Required = true,
+	Options,
+	Default,
+	Callback = function()
+		if UIisLoaded < 2 then 
+			return 
+		end
+
+		if MacLib.Options["mobs_Dropdown"].Value ~= nil then
+
+			saveConfig()
+		end
+	end,
+}, "mobs_Dropdown")
+
+local Section_AutoFarm_Macro = Tab_AutoFarm:Section({ --
+    Side = "Right"
+})
+Section_AutoFarm_Macro:Header({
+	Text = "Macro Mode"
+}, "")
+Section_AutoFarm_Macro:Toggle({ -- Main Auto-Colosseum Toggle
+	Name = "Enable",
+	Default = false,
+	Callback = function()
+		if UIisLoaded < 2 then 
+			return 
+		end
+		
+		if MacLib.Options["AutoFarm_Macro_Toggle"].State then
+			if MacLib.Options["AutoFarm_Bot_Toggle"].State then
+				MacLib.Options["AutoFarm_Bot_Toggle"]:UpdateState(false)
+			end
+			if MacLib.Options["AutoFarm_Toggle"].State then
+				MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+			end
+			saveConfig()
+		elseif not MacLib.Options["AutoFarm_Bot_Toggle"].State and not MacLib.Options["AutoFarm_Macro_Toggle"].State and MacLib.Options["AutoFarm_Toggle"].State then
+			MacLib.Options["AutoFarm_Toggle"]:UpdateState(false)
+			saveConfig()
+		end
+	end,
+}, "AutoFarm_Macro_Toggle")
+Section_AutoFarm_Macro:Slider({
+	Name = "Range",
+	Default = 35,
+	Minimum = 15,
+	Maximum = 100,
+	DisplayMethod = "Value",
+	Callback = function()
+		repeat task.wait() until UIisLoaded == 2
+	end,
+}, "AutoFarm_Range_Slider")
+
+local Tab_AutoDungeon = Group_Function:Tab({ -- Function Auto-Dungeon Tab
+    Name = "Auto-Dungeon",
+    Image = "rbxassetid://91933270767990"
+})
+local Section_AutoDungeon_Difficulty = Tab_AutoDungeon:Section({ --
+    Side = "Left"
+})
+Section_AutoDungeon_Difficulty:Dropdown({
+	Name = "Difficulty",
+	Search = false,
+	Multi = false,
+	Required = true,
+	Options = {"Easy", "Hard"},
+	Default = 1,
+	Callback = function()
+	end,
+}, "AutoDungeon_Difficulty_Dropdown")
+
+local Tab_AutoColosseum = Group_Function:Tab({ -- Function Auto-Dungeon Tab
+    Name = "Auto-Colosseum",
+    Image = "rbxassetid://76200096147550"
+})
+local Section_AutoColosseum_Difficulty = Tab_AutoColosseum:Section({ --
+    Side = "Left"
+})
+Section_AutoColosseum_Difficulty:Dropdown({
+	Name = "Difficulty",
+	Search = false,
+	Multi = false,
+	Required = true,
+	Options = {"Easy"},
+	Default = 1,
+	Callback = function()
+	end,
+}, "AutoColosseum_Difficulty_Dropdown")
+
+local Extra_Settings = Window:TabGroup() -- Settings Group
+local Extra_Settings = Extra_Settings:Tab({ -- Settings Settings Tab
+    Name = "Extra",
+    Image = "rbxassetid://73899994101513"
+})
+local Section_Extra_OpenClones = Tab_AutoColosseum:Section({ --
+    Side = "Left"
+})
+Section_Extra_OpenClones:Dropdown({
+	Name = "Digiclone",
+	Search = false,
+	Multi = false,
+	Required = true,
+	Options = {"Attack", "Defense", "Attack Speed", "Defense", "Critical", "Regeneration"},
+	Default = 1,
+	Callback = function()
+	end,
+}, "")
+
+
+local Group_Settings = Window:TabGroup() -- Settings Group
+local Tab_Settings = Group_Settings:Tab({ -- Settings Settings Tab
+    Name = "Settings",
+    Image = "rbxassetid://73899994101513"
+})
+
+local Section_Settings_Misc = Tab_Settings:Section({
+	Side = "Left"
+})
+Section_Settings_Misc:Toggle({ -- Main Auto-Colosseum Toggle
+	Name = "Check Update",
+	Default = false,
+	Callback = function()
+	end,
+}, "Settings_CheckUpdate_Toggle")
+
+local Section_Settings_Reconnect = Tab_Settings:Section({
+	Side = "Left"
+})
+Section_Settings_Reconnect:Toggle({ -- Main Auto-Colosseum Toggle
+	Name = "Enable Reconnect Timer",
+	Default = false,
+	Callback = function()
+	end,
+}, "Settings_Reconnect_Toggle")
+Section_Settings_Reconnect:Slider({
+	Name = "Webhook Timer",
+	Default = 300,
+	Minimum = 60,
+	Maximum = 3600,
+	DisplayMethod = "Value",
+	Callback = function()
+	end,
+}, "Settings_Reconnect_Slider")
+
+local Section_Settings_Discord = Tab_Settings:Section({
+	Side = "Right"
+})
+Section_Settings_Discord:Toggle({ -- Main Auto-Colosseum Toggle
+	Name = "Enable Discord Webhook",
+	Default = false,
+	Callback = function()
+	end,
+}, "Settings_EnableDiscord_Toggle")
+Section_Settings_Discord:Slider({
+	Name = "Webhook Timer",
+	Default = 60,
+	Minimum = 60,
+	Maximum = 3600,
+	DisplayMethod = "Value",
+	Callback = function()
+	end,
+}, "Settings_WebhookTimer_Slider")
+Section_Settings_Discord:Input({
+	Name = "Discord Webhook",
+	Placeholder = "Webhook URL",
+	AcceptedCharacters = "All",
+	Callback = function()
+	end,
+}, "Settings_WebhookURL_Input")
+
+Window:SetScale(0.7) -- Window Scale
+Tab_Main:Select() -- Select Main Tab
+MacLib:SetFolder("bitsv3rm")
+MacLib:LoadConfig("bitsv3rm")
+
+UIisLoaded = 1
+MacLib.Options["mobs_Dropdown"]:InsertOptions(getgenv().Settings.Enemies[MacLib.Options["world_Dropdown"].Value]["Mobs"])
+
+MacLib:LoadConfig("bitsv3rm")
+UIisLoaded = 2
+
+--- UI ---
+
+-- Anti-AFK
+player.Idled:Connect(function()
+	game:GetService("VirtualUser"):CaptureController()
+	game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
